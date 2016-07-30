952d119b2338b4c6401632a8fb9a1ab843b1e568
cocodoba <tpdhp991>
2016-07-30 23:23:19 +0900

    <FINISH> 起動時に自動でサービスを開始 →manifest: android.permission.RECEIVE_BOOT_COMPLETED →BroadCastReceiverを継承したBootCompletedReceiverで受け取る
    Service
    →TimerとHandler.postをつかって一定秒毎にToastを表示
    
    <NEXT>
    Chromeを使用して30秒経過するごとにToastを表示

--- app/src/main/java/com/example/shinoharanaoki/browserusemonitorservice/MyService.java
+++ app/src/main/java/com/example/shinoharanaoki/browserusemonitorservice/MyService.java
@@ -2,35 +2,23 @@ package com.example.shinoharanaoki.browserusemonitorservice;
 
 import android.app.Service;
 import android.content.Intent;
-<<<<<<< HEAD
 import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
-=======
 import android.os.IBinder;
 import android.util.Log;
->>>>>>> origin/master
-
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MyService extends Service {
     private static final String TAG = "MyService";
 
-<<<<<<< HEAD
-    private static boolean service_started = false;
-
-    private Timer timer = null;
     private int count = 0;//テスト用！！
     private int interval_seconds = 3;
 
     private Handler handler;
-=======
     private Timer timer = null;
-    private int count = 0;//テスト用！！
-    private int interval_seconds = 30;
->>>>>>> origin/master
 
     public MyService() {
     }
@@ -45,8 +33,6 @@ public class MyService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
 
         Log.i(TAG, "onStartCommand: ");
-<<<<<<< HEAD
-
         timer = new Timer();
 
         /**
@@ -55,17 +41,12 @@ public class MyService extends Service {
          * */
         handler = new Handler();
 
-=======
-        timer = new Timer();
-
->>>>>>> origin/master
         /*
         * TEST サービス駆動確認用*/
         timer.schedule( new TimerTask(){
             @Override
             public void run(){
                 Log.d( "Count Test" , "count = "+ count );
-<<<<<<< HEAD
                 handler.post(new Runnable() {
                     @Override
                     public void run() {
@@ -78,22 +59,12 @@ public class MyService extends Service {
 
         //明示的にサービスの起動、停止が決められる場合の返り値
         return START_STICKY;
-=======
-                count++;
-            }
-        }, 0, 1000);
-
-        return super.onStartCommand(intent, flags, startId);
->>>>>>> origin/master
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-<<<<<<< HEAD
         timer.cancel();
-=======
->>>>>>> origin/master
     }
 
     @Override
