dd45eb461b4161f731f649cbbfd51556430430a5
cocodoba <tpdhp991>
2016-08-04 02:31:07 +0900

    基本動作確認版
    現在最前面のアプリを定期的に取得
    limitを超えたら他のアプリを開始
    
    修正予定
    スリープ時にカウントをしない
    limit対象アプリを配列にして拡張forで処理
    
    追加予定

--- app/src/main/java/com/example/shinoharanaoki/browserusemonitorservice/MainActivity.java
+++ app/src/main/java/com/example/shinoharanaoki/browserusemonitorservice/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.shinoharanaoki.browserusemonitorservice;
 
+import android.app.AppOpsManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+
+/**
+ * Lisence: MIT
+ * 実業務でちゃんと使えるAndroidアプリ開発入門（1）：http://www.atmarkit.co.jp/ait/articles/1602/01/news156.html
+ *
+ * Source Code
+ * https://github.com/satosystems/atmarkit-android/blob/master/No01/app/src/main/java/com/example/atmarkit/no01/MainActivity.java
+*/
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private static final int REQUEST_SETTINGS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +29,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_SETTINGS) {
+            startMyService();
+        }
+    }
+
     /*
     * サービス開始ボタン
     */
     public void onStartClick(View view) {
-        Intent i = new Intent(this, MyService.class);
-        startService(i);
+        startMyService();
 
         Log.i(TAG, "onStartClick: ");
 
@@ -35,5 +55,28 @@ public class MainActivity extends AppCompatActivity {
         stopService(i);
 
         Log.i(TAG, "onStopClick: ");
+    }
+
+    private void startMyService() {
+        Intent intent = null;
+        if (!canGetUsageStats()) {
+            intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
+        }
+        if (intent != null) {
+            startActivityForResult(intent, REQUEST_SETTINGS);
+            Toast.makeText(getApplicationContext(), "Please turn ON", Toast.LENGTH_SHORT).show();
+        } else {
+            startService(new Intent(MainActivity.this, MyService.class));
+        }
+    }
+
+    public boolean canGetUsageStats() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            return true;
+        }
+        AppOpsManager aom = (AppOpsManager) getSystemService(APP_OPS_SERVICE);
+        int uid = android.os.Process.myUid();
+        int mode = aom.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS, uid, getPackageName());
+        return mode == AppOpsManager.MODE_ALLOWED;
     }
 }
--- app/src/main/java/com/example/shinoharanaoki/browserusemonitorservice/MyService.java
+++ app/src/main/java/com/example/shinoharanaoki/browserusemonitorservice/MyService.java
@@ -26,19 +26,25 @@ public class MyService extends Service {
     private static final String TAG = "MyService";
 
     private int count = 0;//テスト用！！
-    private int count_interval_seconds = 10;
-    private int usageStats_interval_seconds = 10; //サービスの更新間隔
+    private int count_interval_seconds = 3;
+    private int usageStats_interval_seconds = 10;
 
     private Handler handler;
     private Timer count_timer = null;
     private Timer usage_interval_timer;
 
     private final String chrome_package_name = "com.android.chrome";
-    private int chrome_usagetime_counter; //Chrome使用時間1分につき＋１カウント
-    private final int chrome_use_limit = 5; //Chrome使用制限時間
+    private final String y_news_package_name = "jp.co.yahoo.android.news";
+    private final String youtube_package_name = "com.google.android.youtube";
 
-    private final String[] alternative_apps_packnames = {"com.google.android.calendar","com.google.android.maps"};//途中で起動するアプリのパッケージネーム
+    private int over_use_count;
+    private int limit = 15;
 
+    private final String[] alternative_apps = {"com.moneyforward.android.app",
+                                               "com.nowpro.nar03_f",
+                                               "me.phrase.phrase",
+                                               };
+    private int app_select_num;
 
     public MyService() {
     }
@@ -80,38 +86,42 @@ public class MyService extends Service {
 
         /**
          *
-         * 一定秒毎にUsageStatsを取得してChromeの使用履歴があれば出力する
+         * 一定秒毎にUsageStatsを取得してChromeの使用履歴があればカウントする
          * */
         usage_interval_timer.schedule(new TimerTask() {
             @Override
             public void run() {
                 String now_foreground_app = getTopActivityPackageName();
-                Log.d(TAG, "run: 現在最前面で使用中のアプリ ＝ " + now_foreground_app);
-
-                if (chrome_package_name.equals(now_foreground_app)){
-                    chrome_usagetime_counter++;
-                    Log.d(TAG, "run: chrome_usage_counter = " + chrome_usagetime_counter);
-                    handler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            Toast.makeText(MyService.this, "Chrome Count = " + chrome_usagetime_counter, Toast.LENGTH_SHORT).show();
-                        }
-                    });
-
-                    if(chrome_usagetime_counter >=chrome_use_limit){
+                Log.d(TAG, "run: 現在最前面のアプリ ＝ "+ now_foreground_app);
+                if(now_foreground_app.equals(chrome_package_name)
+                    ||now_foreground_app.equals(y_news_package_name)
+                    ||now_foreground_app.equals(youtube_package_name)){
+                    over_use_count++;
+                    Log.d(TAG, "run: over_use_count = " + over_use_count);
+                    if(over_use_count >= limit) {
                         PackageManager pm = getPackageManager();
-                        Intent intent = pm.getLaunchIntentForPackage("com.google.android.apps.maps");
-                        try{
-                            startActivity(intent);
-                        } catch (Exception e) {
-                            Log.d(TAG, "run: 対象のアプリが見つかりません");
+                        Intent intent = pm.getLaunchIntentForPackage(alternative_apps[app_select_num]);
+                        if(app_select_num == alternative_apps.length-1){
+                            app_select_num = 0;
+                        }else{
+                            app_select_num++;
                         }
-                        chrome_usagetime_counter = 0;
-
+                        try {
+                            startActivity(intent);
+                            handler.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(MyService.this, "limit"+limit+"です", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        } catch (Exception e) {
+                            Log.e(TAG, "run: 指定したアプリは見つかりません", e);
+                        }
+                        over_use_count = 0;
                     }
                 }
             }
-        },0, 1000*usageStats_interval_seconds);
+        },0, 1000 * usageStats_interval_seconds);
 
         //明示的にサービスの起動、停止が決められる場合の返り値
         return START_STICKY;
@@ -130,26 +140,6 @@ public class MyService extends Service {
         throw new UnsupportedOperationException("Not yet implemented");
     }
 
-    /**
-     * OSからアプリ使用履歴情報を取得
-     *
-     * @return Map<Stringパッケージ名, UsageStats使用履歴>
-     */
-    private Map<String, UsageStats> getUsageStatsMap() {
-
-        Map<String, UsageStats> myStatsMap;
-
-        UsageStatsManager mUsageStatsManager = (UsageStatsManager) getSystemService(USAGE_STATS_SERVICE);
-        long time = System.currentTimeMillis();
-
-        // We get usage stats for the last interval
-        myStatsMap = mUsageStatsManager.queryAndAggregateUsageStats(time - 1000*usageStats_interval_seconds, time);
-        // Sort the stats by the last time used
-        if (!myStatsMap.isEmpty()) {Log.i(TAG, "getUsageStatsMap: UsageStatsにアクセスできました");}
-
-        return myStatsMap;
-    }
-
     private String getTopActivityPackageName() {
         String packageName = "";
         if(Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
@@ -165,8 +155,10 @@ public class MyService extends Service {
                 SortedMap<Long, UsageStats> map = new TreeMap<>();
                 for (UsageStats usageStats : list) {
                     map.put(usageStats.getLastTimeUsed(), usageStats);
+                    /*UsageStatsで取得できたアプリ履歴をすべて表示*/
                     Log.d(TAG, "package: " + usageStats.getPackageName());
                 }
+                /*UsageStatsで取得できたアプリ履歴の件数*/
                 Log.d(TAG, "size: " + map.size());
                 if (!map.isEmpty()) {
                     packageName = map.get(map.lastKey()).getPackageName();
@@ -177,6 +169,5 @@ public class MyService extends Service {
 
         return packageName;
     }
-
 
 }
