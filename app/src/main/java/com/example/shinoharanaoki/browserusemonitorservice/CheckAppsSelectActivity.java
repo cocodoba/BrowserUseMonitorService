5748784893d1cc2af33ea1d80a2d5f2464d88deb
cocodoba <tpdhp991>
2016-08-06 17:54:16 +0900

    リストのアイテムを、テスト用のString配列からインストール済みアプリ一覧に戻した
    一番下までスクロールするとアクティビティがフリーズする不具合を修正

--- app/src/main/java/com/example/shinoharanaoki/browserusemonitorservice/CheckAppsSelectActivity.java
+++ app/src/main/java/com/example/shinoharanaoki/browserusemonitorservice/CheckAppsSelectActivity.java
@@ -25,54 +25,40 @@ public class CheckAppsSelectActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_apps_select);
 
-        //final String[] item; //FIXME name
-        final String[] item = new String [] {
-                "listView item 1",
-                "listView item 2",
-                "listView item 3",
-                "listView item 4",
-                "listView item 5",
-                "listView item 6",
-                "listView item 7",
-                "listView item 8",
-                "listView item 9",
-                "listView item 10",
-                "listView item 11",
-                "listView item 12",
-                "listView item 13",
-                "listView item 14",
-                "listView item 15"
-        };
+        final String[] app_names; //FIXME name
 
         /*端末にインストール済のアプリケーション一覧情報を取得*/
-        /*final PackageManager pm = getPackageManager();
+        final PackageManager pm = getPackageManager();
         List<ApplicationInfo> installedAppInfos = pm.getInstalledApplications(0);
-        int i = 1;
+        /*ユーザーが起動できるアプリを抽出*/
         List<ApplicationInfo> selectableAppInfos = new ArrayList<>();
         for(ApplicationInfo info : installedAppInfos){
             if(pm.getLaunchIntentForPackage(info.packageName) != null){
                 Log.i(TAG, "onCreate: " + "AppName = "+ info.loadLabel(pm) + " PackName = " + info.packageName);
                 selectableAppInfos.add(info);
-                i++;
             }
-        }*/
-        /*item = new String[i];
+        }
+        /**
+         * 一番下までスクロールするとアクティビティがフリーズする不具合を修正
+         * */
+        /*ApplicationInfoからアプリ名を取得して配列に格納*/
+        app_names = new String[selectableAppInfos.size()];
         int index = 0;//FIXME indexを使わない方法は？
         for (ApplicationInfo info : selectableAppInfos) {
-            item[index] = (String)info.loadLabel(pm);
-            Log.d(TAG, "onCreate: selectable_App_names = " + "["+index+"]" + item[index]);
+            app_names[index] = (String)info.loadLabel(pm);
+            Log.d(TAG, "onCreate: selectable_App_names = " + "["+index+"]" + app_names[index]);
             index++;
-        }*/
+        }
 
-        // 追加するアイテムを生成する
+        // アイテムをアダプタにセット
         ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                this, android.R.layout.simple_list_item_multiple_choice, item);
+                this, android.R.layout.simple_list_item_multiple_choice, app_names);
 
         // リストビューにアイテム (adapter) を追加
         final ListView listView1 = (ListView)findViewById(R.id.listView1); //FIXME name
         listView1.setAdapter(adapter);
 
-        /**「android ListView 複数リスト　選択した値を取得する”」
+        /**「android ListView 複数リスト 選択した値を取得する”」
          *   http://k-1-ne-jp.blogspot.jp/2013/09/android-listview.html*/
 
         /**
@@ -99,7 +85,7 @@ public class CheckAppsSelectActivity extends AppCompatActivity {
                     //valueAtでチェックされていればtrueが返ってくる
                     if (checked.valueAt(index)) {
                         int key = checked.keyAt(index);//チェックされている配列のキーを取得
-                        sb.append("\""+item[key]+"\"" + ",  ");//もともとの配列から値を取得する
+                        sb.append("\""+app_names[key]+"\"" + ",  ");//もともとの配列から値を取得する
                     }
                 }
                 Toast.makeText( CheckAppsSelectActivity.this, "NOW SELECTING: " + sb.substring(0, sb.length()-1), Toast.LENGTH_LONG ).show();
@@ -131,7 +117,7 @@ public class CheckAppsSelectActivity extends AppCompatActivity {
                 for (int index=0; index<checked.size(); index++) {
                     if (checked.valueAt(index)) {
                         int key_of_list_position = checked.keyAt(index);//リストビューの中での元々の順番を取得
-                        sb.append("\""+item[key_of_list_position]+"\""+  " is String from item["+String.valueOf(key_of_list_position)+"]" + ",  ");
+                        sb.append("\""+app_names[key_of_list_position]+"\""+  " is String from item["+String.valueOf(key_of_list_position)+"]" + ",  ");
                     }
                 }
                 // 通知
